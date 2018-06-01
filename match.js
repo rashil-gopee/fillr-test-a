d45457057562e5b45342b89ed0b8c1f845965c94
Isswarraj Gopee <isswarraj.gopee@gmail.com>
2018-06-01 14:33:04 +1000

    looping through hash

--- match.js
+++ match.js
@@ -1,32 +1,14 @@
 module.exports.match = function(hash) {
 	// Write your solution to Task #2 - Match Metadata task here
-	// var field = 'credit card date';
-	//var regex = new RegExp('^\b(' + field + ')');
-	// var regex = new RegExp('^\b(card)/i');
+	var values = [];
 
-	//var regex = new RegExp('/card/i');
-
-	// var regex = '/card/i';
-	// var string = 'card expire date day';
-
-	// //var res = regex.test(string);
-	// var res = string.match(regex);
-	// console.log(res);
-
-	// console.log(regex.test(string));
 	var field = 'card expire date';
 
-	var str = 'card expire date day';
-	var patt1 = /^\b(card expire date)/i;
-	var result = str.match(patt1);
+	var regex = new RegExp('^\\b(' + field + ')', 'i');
 
-	var regex = new RegExp('^\\b(card expire date)', 'i');
+	for (var key in hash) {
+		if (regex.test(key)) values.push(key);
+	}
 
-	console.log(regex.test(str));
-
-	// for (var key in hash){
-	//   console.log(key);
-	// }
-
-	console.log(result);
+	return values;
 };
